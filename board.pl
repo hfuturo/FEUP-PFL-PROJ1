50337b2cea88e90f010501a42bfd8bee52f25ec8
ritaleite03 <ritaicleite@gmail.com>
2023-10-13 14:24:04 +0100

    Add side coordinates

--- board.pl
+++ board.pl
@@ -1,14 +1,8 @@
 :- use_module(library(lists)).
 
-play :-
-    write('write the size of the board: '),
-    read_number(BoardSize),
-    write('\n'),
-    make_board(BoardSize, Board),
-    print_board(Board, BoardSize),
-    !   % remove output true ? do terminal quando acaba de correr
-.
-
+/*
+    begining teacher code
+*/
 read_number(BoardSize) :-
     read_number_aux(0,false,BoardSize).
 
@@ -21,19 +15,44 @@ read_number_aux(Acc,_,BoardSize) :-
     read_number_aux(Acc1, true, BoardSize)
 .
 read_number_aux(BoardSize,true,BoardSize).
+/*
+    ending teacher code
+*/
 
+program :-
+    initial_state(BoardSize,Board).
+
+/*
+    create board with specific size
+*/
+initial_state(BoardSize,Board) :-
+    write('write the size of the board: '),
+    read_number(BoardSize),
+    write('\n'),
+    make_board(BoardSize, Board),
+    print_board(Board, BoardSize),
+    !.   % remove output true ? do terminal quando acaba de correr
+
+/*
+    create board with 0
+*/
 make_board(BoardSize, Board) :-
     length(Board, BoardSize),
     length(Row, BoardSize),
     maplist(=(0), Row),
-    maplist(=(Row), Board)
-.
+    maplist(=(Row), Board).
 
+/*
+    print board in the console
+*/
 print_board(Board, BoardSize) :-
     print_board_top_coordinates(BoardSize),
     write('\n'),
-    print_board_content(Board, BoardSize).
+    print_board_content(Board, BoardSize, 1).
 
+/*
+    print top coordinates of the board
+*/
 print_board_top_coordinates(BoardSize) :-
     print_board_top_coordinates_aux(BoardSize, 1).
 
@@ -47,22 +66,37 @@ print_board_top_coordinates_aux(BoardSize, CurrentCoordinate) :-
     CurrentCoordinate1 is CurrentCoordinate + 1,
     print_board_top_coordinates_aux(BoardSize1, CurrentCoordinate1). 
 
-print_board_content([], BoardSize) :- 
+/*
+    print content of the board
+*/
+print_board_content([], BoardSize,_) :- 
     print_limiter(_,BoardSize),
     write('\n').
-print_board_content([H|T], BoardSize) :-
+print_board_content([H|T], BoardSize,Line) :-
     print_limiter(_, BoardSize),
-    print_line(H),
-    print_board_content(T, BoardSize).
+    print_line(H,Line),
+    Line1 is Line+1,
+    print_board_content(T, BoardSize,Line1).
 
 
-print_line([]) :- 
-    write('|\n').
-print_line([H|T]) :-
+/*
+    print line of the board
+*/
+print_line(Content,Line) :-
+    print_line_content(Content),
+    format('| ~w\n',Line).
+
+/*
+    print content of the line of the board
+*/
+print_line_content([]).
+print_line_content([H|T]) :-
     format('| ~w ',H),
-    print_line(T).
+    print_line_content(T).
 
-
+/*
+    create squares to separate the content of the board
+*/
 print_limiter(_, BoardSize) :-
     print_limiter_aux(0, BoardSize).
 
