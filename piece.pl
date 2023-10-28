6b2fb12d7abc487dafc20047b7e3666e35cb5449
Henrique Silva <hggfsilva@gmail.com>
2023-10-28 03:19:04 +0100

    added diagonal NE-SW

--- piece.pl
+++ piece.pl
@@ -13,13 +13,80 @@ choose_move(Turn,Height,Width,Board,XP,YP,XM,YM) :-
 
 /*
     calcula todas as distancias em todas as direcoes que a peça consegue percorrer
-    Distances vai retornar uma lista com as distancias nas seguintes direcoes [vertical,horizontal,diagonal]
+    Distances vai retornar uma lista com as distancias nas seguintes direcoes [vertical,horizontal,diagonalNE-SW,diagonalNW-SE]
 */
 calculate_distances(X,Y,Turn,Height,Width,Board,Distances) :-
     row_distance(X,Y,Board,Width,Turn,RowDistance),
     column_distance(X,Y,Board,Height,Turn,ColumnDistance),
-    append([ColumnDistance],[RowDistance],Distances),
+    diagonal_distance_NESW(X,Y,Board,Width,Height,Turn,DiagonalDistance),
+    append([ColumnDistance],[RowDistance],DistancesAux),
     write(Distances).
+
+/*
+    calcula a distancia que a peca pode correr na diagonal de NE-SW
+*/
+diagonal_distance_NESW(X,Y,Board,Width,Height,Turn,DiagonalDistance) :-
+    nth1(Y,Board,Row),
+    nth1(X,Row,XValue),
+    Times is 0,
+    diagonal_distance_NE(X,Y,XValue,Board,Width,Turn,Times,NEDiagonalDistance),  % ↗
+    diagonal_distance_SW(X,Y,XValue,Board,Height,Turn,Times,SWDiagonalDistance), % ↙
+    DiagonalDistance is NEDiagonalDistance + SWDiagonalDistance - 1,
+    write(DiagonalDistance).
+
+/*
+    calcula o numero de peças da mesma equipa seguidas que estão na diagonal de NE
+*/
+diagonal_distance_NE(Width,_,_,_,Width,_,0,1) :- !. % peça encontra-se encostada a board na parte do lado direito
+diagonal_distance_NE(_,1,_,_,_,_,0,1) :- !.     % peça encontra-se encostada a board em cima
+diagonal_distance_NE(Width,_,1,_,Width,1,_,1) :- !. % jogador 1 encontra 1 no fim da board do lado direito
+diagonal_distance_NE(_,1,1,_,_,1,_,1) :- !.     % jogador 1 encontra 1 no fim da board em cima
+diagonal_distance_NE(Width,_,2,_,Width,2,_,1) :- !. % jogador 2 encontra 2 no fim da board do lado direito
+diagonal_distance_NE(_,1,2,_,_,2,_,1) :- !.     % jogador 2 encontra 2 no fim da board em cima
+diagonal_distance_NE(Width,_,_,_,Width,_,_,0).      % jogador 1/2 encontra 0,2/0,1 no fim da board do lado direito
+diagonal_distance_NE(_,1,_,_,_,_,_,0).          % jogador 1/2 encontra 0,2/0,1 no fim da board em cima
+diagonal_distance_NE(_,_,0,_,_,1,_,0).              % jogador 1 encontra 0
+diagonal_distance_NE(_,_,2,_,_,1,_,0).              % jogador 1 encontra 2
+diagonal_distance_NE(_,_,0,_,_,2,_,0).              % jogador 2 encontra 0
+diagonal_distance_NE(_,_,1,_,_,2,_,0).              % jogador 2 encontra 1
+diagonal_distance_NE(X,Y,XValue,Board,Width,Turn,Times,Distance) :-
+    X =< Width,
+    Y >= 1,
+    XValue is Turn,
+    UpdatedX is X + 1,
+    UpdatedY is Y - 1,
+    UpdatedTimes is Times + 1,
+    nth1(UpdatedY,Board,Row),
+    nth1(UpdatedX,Row,UpdatedXVal),
+    diagonal_distance_NE(UpdatedX,UpdatedY,UpdatedXVal,Board,Width,Turn,UpdatedTimes,UpdatedDistance),
+    Distance is UpdatedDistance + 1.
+
+/*
+    calcula o numero de peças da mesma equipa seguidas que estão na diagonal de SW
+*/
+diagonal_distance_SW(1,_,_,_,_,_,0,1) :- !.        % peça encontra-se encostada a board na parte do lado esquerdo
+diagonal_distance_SW(_,Height,_,_,Height,_,0,1) :- !.   % peça encontra-se encostada a board em baixo
+diagonal_distance_SW(1,_,1,_,_,1,_,1) :- !.        % jogador 1 encontra 1 no fim da board do lado esquerdo
+diagonal_distance_SW(_,Height,1,_,Height,1,_,1) :- !.   % jogador 1 encontra 1 no fim da board em baixo
+diagonal_distance_SW(1,_,2,_,_,2,_,1) :- !.        % jogador 2 encontra 2 no fim da board do lado esquerdo
+diagonal_distance_SW(_,Height,2,_,Height,2,_,1) :- !.   % jogador 2 encontra 2 no fim da board em baixo
+diagonal_distance_SW(1,_,_,_,_,_,_,0).             % jogador 1/2 encontra 0,2/0,1 no fim da board do lado esquerdo
+diagonal_distance_SW(_,Height,_,_,Height,_,_,0).        % jogador 1/2 encontra 0,2/0,1 no fim da board em baixo
+diagonal_distance_SW(_,_,0,_,_,1,_,0).                  % jogador 1 encontra 0
+diagonal_distance_SW(_,_,2,_,_,1,_,0).                  % jogador 1 encontra 2
+diagonal_distance_SW(_,_,0,_,_,2,_,0).                  % jogador 2 encontra 0
+diagonal_distance_SW(_,_,1,_,_,2,_,0).                  % jogador 2 encontra 1
+diagonal_distance_SW(X,Y,XValue,Board,Height,Turn,Times,Distance) :-
+    X >= 1,
+    Y =< Height,
+    XValue is Turn,
+    UpdatedX is X - 1,
+    UpdatedY is Y + 1,
+    UpdatedTimes is Times + 1,
+    nth1(UpdatedY,Board,Row),
+    nth1(UpdatedX,Row,UpdatedXVal),
+    diagonal_distance_SW(UpdatedX,UpdatedY,UpdatedXVal,Board,Height,Turn,UpdatedTimes,UpdatedDistance),
+    Distance is UpdatedDistance + 1.
 
 /*
     calcula a distancia que a peça pode percorrer verticalemente
