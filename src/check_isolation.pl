c89a0c0ce19393fc1f1396a8783a59ee7bb0d062
ritaleite03 <ritaicleite@gmail.com>
2023-11-02 22:22:44 +0000

    final clean up?

--- src/check_isolation.pl
+++ src/check_isolation.pl
@@ -1,63 +1,8 @@
 /*
-    Verifica se há algum continuous jump que se pode fazer, e se sim diz preenche XM e YM com as coordenadas da nova posição
-    check_isolation_jump(+GameState,+X,+Y,-XM,-YM,-Min,+VisitedPositions)
-*/
-check_isolation_jump(GameState,X,Y,XM,YM,Min,VisitedPositions) :-
-    calculate_distances(X,Y,GameState,Distances),
-    nth1(1,Distances,JumpColumn),
-    nth1(2,Distances,JumpRow),
-    nth1(3,Distances,JumpNESW),
-    nth1(4,Distances,JumpNWSE),
-
-    XLeft is X-JumpRow, 
-    XRight is X+JumpRow,
-    YUp is Y-JumpColumn, 
-    YDown is Y+JumpColumn,
-
-    XSW is X-JumpNESW, 
-    XNE is X+JumpNESW, 
-    YNE is Y-JumpNESW, 
-    YSW is Y+JumpNESW,
-    XNW is X-JumpNWSE, 
-    XSE is X+JumpNWSE, 
-    YNW is Y-JumpNWSE, 
-    YSE is Y+JumpNWSE,
-
-    check_isolation_move(XLeft,Y,Value1,GameState,1),
-    check_isolation_move(XRight,Y,Value2,GameState,1),
-    check_isolation_move(X,YUp,Value3,GameState,1),
-    check_isolation_move(X,YDown,Value4,GameState,1),
-    check_isolation_move(XNE,YNE,Value5,GameState,1),
-    check_isolation_move(XSW,YSW,Value6,GameState,1),
-    check_isolation_move(XNW,YNW,Value7,GameState,1),
-    check_isolation_move(XSE,YSE,Value8,GameState,1),
-
-    append([[Value1,XLeft,Y,JumpColumn],[Value2,XRight,Y,JumpColumn],[Value3,X,YUp,JumpRow],[Value4,X,YDown,JumpRow],[Value5,XNE,YNE,JumpNESW],[Value6,XSW,YSW,JumpNESW],[Value7,XNW,YNW,JumpNWSE],[Value8,XSE,YSE,JumpNWSE]],[],Moves),
-    findall(
-        [Value, XT, YT], 
-        (
-            member([Value,XT,YT], Moves), 
-            \+ member([XT, YT], VisitedPositions),
-            check_inside_board(XT,YT,GameState),
-            \+no_jump(X,Y,XT,YT),
-            \+get_position_player(XT,YT,GameState)
-        ),
-        PossibleMoves
-    ),
-    length(PossibleMoves,Number),
-    Number =\= 0,
-    !,
-    sort(PossibleMoves,SortedPossibleMoves),
-    nth1(1,SortedPossibleMoves,Elem),
-    nth1(1,Elem,Min),nth1(2,Elem,XM),nth1(3,Elem,YM).
-
-check_isolation_jump(_,_,_,XM,YM,Min,_) :- XM is 0, YM is 0, Min is 8.
-
-/*
     Calcula o nivel de isolamento que se pode atingir com cada movimento possivel a partir de uma peça, preenchendo XM e YM com as coordenadas da mulher opção
-    check_isolation_piece(+GameState,+X,+Y,-XM,-YM) :-
+    best_piece_move(+GameState,+X,+Y,-XM,-YM) :-
 */
-check_isolation_piece(GameState,X,Y,XM,YM) :-
+best_piece_move(GameState,X,Y,XM,YM) :-
     calculate_distances(X,Y,GameState,Distances),
     nth1(1,Distances,JumpColumn),
     nth1(2,Distances,JumpRow),
@@ -78,14 +23,14 @@ check_isolation_piece(GameState,X,Y,XM,YM) :-
     YNW is Y-JumpNWSE, 
     YSE is Y+JumpNWSE,
 
-    check_isolation_move(XLeft,Y,Value1,GameState,1),
-    check_isolation_move(XRight,Y,Value2,GameState,1),
-    check_isolation_move(X,YUp,Value3,GameState,1),
-    check_isolation_move(X,YDown,Value4,GameState,1),
-    check_isolation_move(XNE,YNE,Value5,GameState,1),
-    check_isolation_move(XSW,YSW,Value6,GameState,1),
-    check_isolation_move(XNW,YNW,Value7,GameState,1),
-    check_isolation_move(XSE,YSE,Value8,GameState,1),
+    get_piece_isolation(XLeft,Y,Value1,GameState,1),
+    get_piece_isolation(XRight,Y,Value2,GameState,1),
+    get_piece_isolation(X,YUp,Value3,GameState,1),
+    get_piece_isolation(X,YDown,Value4,GameState,1),
+    get_piece_isolation(XNE,YNE,Value5,GameState,1),
+    get_piece_isolation(XSW,YSW,Value6,GameState,1),
+    get_piece_isolation(XNW,YNW,Value7,GameState,1),
+    get_piece_isolation(XSE,YSE,Value8,GameState,1),
     
     append([[XLeft,Y],[XRight,Y],[X,YUp],[X,YDown],[XNE,YNE],[XSW,YSW],[XNW,YNW],[XSE,YSE]],[],Moves),
     append([Value1,Value2,Value3,Value4,Value5,Value6,Value7,Value8],[],Values),
@@ -115,42 +60,41 @@ check_isolation_piece(GameState,X,Y,XM,YM) :-
         -> util para quando se usa este predicado para calcular o nivel de isolamento de um movimento, que caso seja para uma "casa" ocupada por uma peça do mesmo jogador, torna-se impossivel e por isso define-se como isolamento minimo;
     Se o Bool for 0, não se verifica isto
         -> util para descobrir qual a peça menos ou mais isolada.
-    check_isolation_move(+X,+Y,-Value,+GameState,+Bool) 
+    get_piece_isolation(+X,+Y,-Value,+GameState,+Bool) 
 */
-check_isolation_move(X,Y,Value,GameState,_) :-
+get_piece_isolation(X,Y,Value,GameState,_) :-
     \+check_inside_board(X,Y,GameState),
     Value is 8,
     !.
 
-check_isolation_move(X,Y,Value,(Board,Turn,_),1) :- 
-    get_position_piece_check(X,Y,(Board,Turn,_),Piece),
+get_piece_isolation(X,Y,Value,(Board,Turn,_),1) :- 
+    get_piece_value(X,Y,(Board,Turn,_),Piece),
     Piece is Turn,
     Value is 8,
     !.
 
-check_isolation_move(X,Y,Value,(Board,Turn,_),_) :-
+get_piece_isolation(X,Y,Value,(Board,Turn,_),_) :-
     XM is X-1,
     XP is X+1,
     YM is Y-1,
     YP is Y+1,
 
     /* row */
-    get_position_piece_check(XM,Y,Board,Piece1),
-    get_position_piece_check(XP,Y,Board,Piece2),
+    get_piece_value(XM,Y,Board,Piece1),
+    get_piece_value(XP,Y,Board,Piece2),
 
     /* column */
-    get_position_piece_check(X,YM,Board,Piece3),
-    get_position_piece_check(X,YP,Board,Piece4),
+    get_piece_value(X,YM,Board,Piece3),
+    get_piece_value(X,YP,Board,Piece4),
 
     /* diagonal */
-    get_position_piece_check(XM,YM,Board,Piece5),
-    get_position_piece_check(XP,YP,Board,Piece6),
-    get_position_piece_check(XM,YP,Board,Piece7),
-    get_position_piece_check(XP,YM,Board,Piece8),
+    get_piece_value(XM,YM,Board,Piece5),
+    get_piece_value(XP,YP,Board,Piece6),
+    get_piece_value(XM,YP,Board,Piece7),
+    get_piece_value(XP,YM,Board,Piece8),
 
     append([Piece1,Piece2,Piece3,Piece4,Piece5,Piece6,Piece7,Piece8],[],Isolation),
     delete(Isolation,Turn,NewIsolation),
-
     length(Isolation,Number),
     length(NewIsolation,NewNumber),
     Value is Number-NewNumber,
@@ -158,10 +102,10 @@ check_isolation_move(X,Y,Value,(Board,Turn,_),_) :-
 
 /*
     Indica o valor da peça que se encontra nas coordenadas X e Y, sendo 0 caso a peça esteja fora do tabuleiro
-    get_position_piece_check(+X,+Y,+Board,-Piece)
+    get_piece_value(+X,+Y,+Board,-Piece)
 */
-get_position_piece_check(X,Y,Board,Piece) :-
+get_piece_value(X,Y,Board,Piece) :-
     check_inside_board(X,Y,(Board,_,_)),
     get_position_piece(X,Y,Board,Piece).
 
-get_position_piece_check(_,_,_,0).
+get_piece_value(_,_,_,0).
--- src/move.pl
+++ src/move.pl
@@ -36,12 +36,9 @@ select_move(XM,YM,GameState) :-
 
 /*
     Retorna todas as jogadas possiveis
-    valid_moves(+GameState,+Player,-ListOfMoves)
+    valid_moves(+GameState,+VisitedPositions,-ListOfMoves)
 */
-
-% person ou Easy AI
-valid_moves(GameState,Player,ListOfMoves) :-
-    ((Player is 1);(Player is 2)),
+valid_moves(GameState,VisitedPositions,ListOfMoves) :-
     board_size(Height,Width,GameState),
     findall(
         [XP,YP],
@@ -57,37 +54,11 @@ valid_moves(GameState,Player,ListOfMoves) :-
             between(1, Width, XM), between(1, Height, YM), 
             member([XP,YP],PlayerPiece),
             \+get_position_player(XM,YM,GameState),
-            check_move_possible(XP,YP,XM,YM,GameState)
+            check_move_possible(XP,YP,XM,YM,GameState),
+            \+member([XM,YM],VisitedPositions)
         ), 
         ListOfMoves
     ).
-
-% Difficult AI
-valid_moves(GameState,3,ListOfMoves) :-
-    board_size(Height,Width,GameState),
-    findall(
-        [Value,X,Y], 
-        (
-            between(1, Width, X), 
-            between(1, Height, Y), 
-            get_position_player(X,Y,GameState),
-            check_isolation_move(X,Y,Value,GameState,0)
-        ), 
-        Pieces
-    ),
-    sort(Pieces, SortedPieces),
-    length(SortedPieces,MaxIndex),
-    nth1(MaxIndex,SortedPieces,Elem),
-    nth1(1,Elem,MaxValue),
-    findall(
-        [Value,X,Y],
-        (
-            member([Value,X,Y],Pieces),
-            Value is MaxValue
-        ),
-        PossibleMoves
-    ),
-    sort(PossibleMoves,ListOfMoves).
 
 /*
     Escolhe uma move de todas as jogadas possiveis
@@ -98,7 +69,7 @@ valid_moves(GameState,3,ListOfMoves) :-
 choose_move(GameState,VisitedPositions,1,(XP,YP,XM,YM)) :-
     length(VisitedPositions,Size),
     Size is 0,
-    valid_moves(GameState,1,ListOfMoves),
+    valid_moves(GameState,VisitedPositions,ListOfMoves),
     repeat,
     select_piece(XP,YP,GameState),
     select_move(XM,YM,GameState),
@@ -114,11 +85,10 @@ choose_move(GameState,VisitedPositions,1,(XP,YP,XM,YM)) :-
     nth1(1,LastPiece,XP),
     nth1(2,LastPiece,YP),
 
-    valid_moves(GameState,1,ListOfMoves),
+    valid_moves(GameState,VisitedPositions,ListOfMoves),
     repeat,
     select_move(XM,YM,GameState),
     member([XP,YP,XM,YM],ListOfMoves),
-    \+member([XM,YM],VisitedPositions),
     \+no_jump(XP,YP,XM,YM),
     !.
 
@@ -127,7 +97,7 @@ choose_move(GameState,VisitedPositions,2,(XP,YP,XM,YM)) :-
     length(VisitedPositions,Size),
     Size is 0,
 
-    valid_moves(GameState,2,ListOfMoves),
+    valid_moves(GameState,VisitedPositions,ListOfMoves),
     length(ListOfMoves,MaxIndex),
     MaxIndexRandom is MaxIndex+1,
     random(1,MaxIndexRandom,Index),
@@ -148,7 +118,7 @@ choose_move(GameState,VisitedPositions,2,(XP,YP,XM,YM)) :-
     nth1(1,LastPiece,XP),
     nth1(2,LastPiece,YP),
 
-    valid_moves(GameState,2,ListOfMoves),
+    valid_moves(GameState,VisitedPositions,ListOfMoves),
     board_size(Height,Width,GameState),
 
     findall(
@@ -157,7 +127,6 @@ choose_move(GameState,VisitedPositions,2,(XP,YP,XM,YM)) :-
             between(1, Width, XM), 
             between(1, Height, YM), 
             member([XP,YP,XM,YM],ListOfMoves),
-            \+member([XM,YM],VisitedPositions),
             \+no_jump(XP,YP,XM,YM)
         ),
         Result
@@ -172,19 +141,77 @@ choose_move(GameState,VisitedPositions,2,(XP,YP,XM,YM)) :-
     nth1(2,Move,YM),
     !.
 
-% modo Difficult AI
-choose_move(GameState,_,3,(XP,YP,XM,YM)) :-
-    valid_moves(GameState,3,ListOfMoves),
+% modo Difficult AI, sem continuous jump
+choose_move(GameState,VisitedPositions,3,(XP,YP,XM,YM)) :-
+    length(VisitedPositions,Size),
+    Size is 0,
 
-    length(ListOfMoves,PossibleMovesLength),
-    UpdatedPossibleMovesLength is PossibleMovesLength + 1,
-    random(1,UpdatedPossibleMovesLength,RandomMove),
+    valid_moves(GameState,VisitedPositions,ListOfMoves),
+    board_size(Height,Width,GameState),
+    findall(
+        [Value,XPT,YPT,XMT,YMT],
+        (
+            between(1, Width, XPT), between(1, Height, YPT), 
+            between(1, Width, XMT), between(1, Height, YMT), 
+            member([XPT,YPT,XMT,YMT],ListOfMoves),
+            get_piece_isolation(XPT,YPT,Value,GameState,0),
+            best_piece_move(GameState,XPT,YPT,XMT,YMT)
+        ),
+        Result
+    ),
 
-    nth1(RandomMove,ListOfMoves,SelectedMove),
+    sort(Result, SortedResult),
+    length(SortedResult,MaxIndex),
+
+    nth1(MaxIndex,SortedResult,Elem),
+    nth1(1,Elem,MaxValue),
+
+    findall(
+        [Value,XPT,YPT,XMT,YMT],
+        (
+            member([Value,XPT,YPT,XMT,YMT],Result),
+            Value is MaxValue
+        ),
+        BestMoves
+    ),
+    length(BestMoves,NumberMoves),
+    UpdatedNumberMoves is NumberMoves + 1,
+    random(1,UpdatedNumberMoves,RandomMove),
+
+    nth1(RandomMove,BestMoves,SelectedMove),
     nth1(2,SelectedMove,XP),
     nth1(3,SelectedMove,YP),
-    check_isolation_piece(GameState,XP,YP,XM,YM).
+    nth1(4,SelectedMove,XM),
+    nth1(5,SelectedMove,YM).
 
+% modo Difficult AI, com continuous jump
+choose_move(GameState,VisitedPositions,3,(XP,YP,XM,YM)) :-
+    length(VisitedPositions,Size),
+    Size > 0,
+
+    nth1(Size,VisitedPositions,LastPiece),
+    nth1(1,LastPiece,XP),
+    nth1(2,LastPiece,YP),
+    get_piece_isolation(XP,YP,Atual,GameState,0),
+
+    valid_moves(GameState,VisitedPositions,ListOfMoves),
+    board_size(Height,Width,GameState),
+    findall(
+        [Value,XMT,YMT],
+        (
+            between(1, Width, XMT), between(1, Height, YMT), 
+            member([XP,YP,XMT,YMT],ListOfMoves),
+            get_piece_isolation(XMT,YMT,Value,GameState,0)
+        ),
+        Result
+    ),
+    sort(Result,SortedResult),
+    nth1(1,SortedResult,Elem),
+    nth1(1,Elem,Min),
+    Atual>Min,
+    !,
+    nth1(2,Elem,XM),
+    nth1(3,Elem,YM).
 
 /*
     Verifica se é possivel fazer um continuous jump, e sim chama os predicados necessários
@@ -198,7 +225,7 @@ check_continuous_jump_cycle((XP,YP,XM,YM),(Board,Turn,TotalMoves),NewGameState,V
         \+check_winner(Board,1,NewTurn)
     ),
     append(VisitedPositions,[[XM,YM]],NewVisitedPositions),
-    valid_moves((Board,Turn,TotalMoves),1,ListOfMoves),
+    valid_moves((Board,Turn,TotalMoves),VisitedPositions,ListOfMoves),
     board_size(Height,Width,(Board,Turn,TotalMoves)),
     findall(
         [NXM,NYM],
@@ -206,7 +233,6 @@ check_continuous_jump_cycle((XP,YP,XM,YM),(Board,Turn,TotalMoves),NewGameState,V
             between(1, Width, NXM), 
             between(1, Height, NYM), 
             member([XM,YM,NXM,NYM],ListOfMoves),
-            \+member([NXM,NYM],NewVisitedPositions),
             \+no_jump(XM,YM,NXM,NYM)
         ),
         Result
@@ -214,54 +240,42 @@ check_continuous_jump_cycle((XP,YP,XM,YM),(Board,Turn,TotalMoves),NewGameState,V
     length(Result,Size),
     Size>0,
     !,
-    do_continuous_jump_cycle(XM,YM,(Board,Turn,TotalMoves),NewGameState,NewVisitedPositions,Type).
+    do_continuous_jump_cycle((Board,Turn,TotalMoves),NewGameState,NewVisitedPositions,Type).
 
 check_continuous_jump_cycle(_,GameState,GameState,_,_).
 
 /*
     Faz um continuous jump se for a vontade do jogador
-    do_continuous_jump_cycle(+XM,+YM,+GameState,-NewGameState,+VisitedPositions,+Type)
+    do_continuous_jump_cycle(+GameState,-NewGameState,+VisitedPositions,+Type)
 */
 /* modo pessoa */
-do_continuous_jump_cycle(_,_,GameState,NewGameState,VisitedPositions,1) :-
+do_continuous_jump_cycle(GameState,NewGameState,VisitedPositions,1) :-
     display_game(GameState),
     menu_jump_cycle(Option,1),
     Option is 1,
     !,
     nl,
-
     choose_move(GameState,VisitedPositions,1,Move),
-
     move(GameState,Move,TempGameState),
     check_continuous_jump_cycle(Move,TempGameState,NewGameState,VisitedPositions,1).
 
 /* modo easy ai */
-do_continuous_jump_cycle(_,_,GameState,NewGameState,VisitedPositions,2) :-
+do_continuous_jump_cycle(GameState,NewGameState,VisitedPositions,2) :-
     menu_jump_cycle(Option,2),
     Option is 1,
     !,
     display_game(GameState),
     nl,
-
     choose_move(GameState,VisitedPositions,2,Move),
-
     move(GameState,Move,TempGameState),
     check_continuous_jump_cycle(Move,TempGameState,NewGameState,VisitedPositions,2).
 
 /* modo difficult ai */
-do_continuous_jump_cycle(XM,YM,GameState,NewGameState,VisitedPositions,3) :-
-    check_isolation_move(XM,YM,Isolation,GameState,0),
-    check_isolation_jump(GameState,XM,YM,NXM,NYM,Min,VisitedPositions),
-    format('XM: ~w  YM: ~w~nNXM: ~w  NYM: ~w~n',[XM,YM,NXM,NYM]),
-    format('Isolation: ~w  Min: ~w~n',[Isolation,Min]),
-    (
-        NXM =\= 0,
-        NYM =\=0,
-        Isolation>Min
-    ),
+do_continuous_jump_cycle(GameState,NewGameState,VisitedPositions,3) :-
+    choose_move(GameState,VisitedPositions,3,Move),
     !,
     display_game(GameState),
-    move(GameState,(XM,YM,NXM,NYM),TempGameState),
-    check_continuous_jump_cycle((XM,YM,NXM,NYM),TempGameState,NewGameState,VisitedPositions,3).
+    move(GameState,Move,TempGameState),
+    check_continuous_jump_cycle(Move,TempGameState,NewGameState,VisitedPositions,3).
 
-do_continuous_jump_cycle(_,_,GameState,GameState,_,_).
+do_continuous_jump_cycle(GameState,GameState,_,_).
